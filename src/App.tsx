8761157444b12ee18c43bc0d764fcf37527d2725
Young-Ho Kim <yghokim@umd.edu>
2020-03-29 23:20:52 -0400

    migrated to use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import { Header } from 'components/Header';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link,
