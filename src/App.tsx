c06b55783e3780ceba7d4de9bc462a47e4e58224
Young-Ho Kim <yghokim@umd.edu>
2020-03-29 21:04:15 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logotype.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" width="300" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Multimodal Mobile App for Exploring Fitness Data using Speech + Touch Interaction
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
