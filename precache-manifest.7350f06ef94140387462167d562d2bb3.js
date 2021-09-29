fb25ffbc476b482f1640efb1d8a6938a7bb9cd6b
Young-Ho Kim <yghokim@umd.edu>
2021-09-29 11:43:53 -0400

    Updates

---
+++ precache-manifest.7350f06ef94140387462167d562d2bb3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9f72866467b57771dbdda001b2f972",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b3ca5eedb032e1765f",
+    "url": "/static/css/main.18acbd7a.chunk.css"
+  },
+  {
+    "revision": "36b59188d9d13791f051",
+    "url": "/static/js/2.b219c3da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b219c3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b3ca5eedb032e1765f",
+    "url": "/static/js/main.5a317579.chunk.js"
+  },
+  {
+    "revision": "cbd85dd62aeb9936d83a",
+    "url": "/static/js/runtime-main.f6157b90.js"
+  },
+  {
+    "revision": "38d5e351a7a3ce9de957dc264876241d",
+    "url": "/static/media/amanda_portrait.38d5e351.jpg"
+  },
+  {
+    "revision": "d29b39dc356a80acdff06704ad60d5e8",
+    "url": "/static/media/author_bongshin.d29b39dc.png"
+  },
+  {
+    "revision": "8530d3d4da7c501186e9a2d81b1b3722",
+    "url": "/static/media/choe_portrait.8530d3d4.png"
+  },
+  {
+    "revision": "61dec3e705dd2918339c3cfd8828fdeb",
+    "url": "/static/media/david_portrait.61dec3e7.jpg"
+  },
+  {
+    "revision": "3fa942c4d5eec941770d4d88fdb2c355",
+    "url": "/static/media/diana_portrait.3fa942c4.jpg"
+  },
+  {
+    "revision": "f81a174ba57f1879c4272755aeafcb4b",
+    "url": "/static/media/github.f81a174b.svg"
+  },
+  {
+    "revision": "9b4cb321a574d58bf32192089fc82f7e",
+    "url": "/static/media/hernisa_portrait.9b4cb321.jpg"
+  },
+  {
+    "revision": "d00f0a0a98dc27e4f818b5f6674edff1",
+    "url": "/static/media/margaret_portrait.d00f0a0a.jpg"
+  },
+  {
+    "revision": "3cd370bb86b09e103eab0b588a3e8845",
+    "url": "/static/media/mymove_horizontal_light.3cd370bb.svg"
+  },
+  {
+    "revision": "a9dbe98dbba0c601ab4bff63fc8a6aff",
+    "url": "/static/media/sabahat_portrait.a9dbe98d.jpg"
+  },
+  {
+    "revision": "cb5d642b758eb86aeb5fc806d6f46e7c",
+    "url": "/static/media/yhkim_portrait.cb5d642b.jpg"
+  }
+]);
